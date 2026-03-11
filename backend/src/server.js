5a656e39075bd5c1f9db0172bd364cae34dea259
quochuyIT03 <nguyenquochuy3102@gmail.com>
2026-03-11 08:55:23 +0700

    push code len git va vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,31 @@ import tasksRoute from "./routes/tasksRoute.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from 'cors'; 
+import path from "path"
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 // middlewares
-app.use(cors({origin: "http://localhost:5173"}))
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({origin: "http://localhost:5173"}))
+}
 
 app.use(express.json());
 
-app.use("/api/tasks", tasksRoute);
+app.use("/api/tasks", tasksRoute); 
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) =>{
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+})
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
